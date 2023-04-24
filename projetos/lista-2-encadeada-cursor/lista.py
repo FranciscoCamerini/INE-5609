d4f89fa97f834f101c93944ca65a3e4647e96757
Francisco Camerini <franciscocamerini@onsign.tv>
2023-04-24 11:23:09 -0300

    Transforma métodos de cursor como públicos

--- projetos/lista-2-encadeada-cursor/lista.py
+++ projetos/lista-2-encadeada-cursor/lista.py
@@ -24,63 +24,62 @@ class Lista:
 
         return string + ']'
 
-    def __avanca_cursor(self, pos: int = 1) -> None:
-        for _ in range(pos):
-            if proximo := self.__cursor.proximo:
-                self.__cursor = proximo
-
-    def __retrocede_cursor(self, pos: int = 1) -> None:
-        for _ in range(pos):
-            if anterior := self.__cursor.anterior:
-                self.__cursor = anterior
-
-    def __move_cursor_posicao(self, pos) -> None:
-        indice_ultimo = self.__n_elementos - 1
-
-        if pos < indice_ultimo / 2 and pos >= 0:
-            self.__move_cursor_inicio()
-            self.__avanca_cursor(pos)
-        else:
-            self.__move_cursor_final()
-            if pos > 0:
-                self.__retrocede_cursor(indice_ultimo - pos)
-            else:
-                pos *= -1
-                self.__retrocede_cursor(pos -  1)
-
-    def __move_cursor_inicio(self) -> None:
-        self.__cursor = self.__primeiro
-
-    def __move_cursor_final(self) -> None:
-        self.__cursor = self.__ultimo
-
     def __avisa_se_cheia(self) -> ListaCheiaException:
         if self.cheia():
             raise ListaCheiaException()
 
-    def __proximo_cursor(self) -> any:
+    def avanca_cursor(self, pos: int = 1) -> None:
+        for _ in range(pos):
+            if proximo := self.__cursor.proximo:
+                self.__cursor = proximo
+
+    def retrocede_cursor(self, pos: int = 1) -> None:
+        for _ in range(pos):
+            if anterior := self.__cursor.anterior:
+                self.__cursor = anterior
+
+    def move_cursor_posicao(self, pos) -> None:
+        indice_ultimo = self.__n_elementos - 1
+
+        if pos < indice_ultimo / 2 and pos >= 0:
+            self.move_cursor_inicio()
+            self.avanca_cursor(pos)
+        else:
+            self.move_cursor_final()
+            if pos > 0:
+                self.retrocede_cursor(indice_ultimo - pos)
+            else:
+                pos *= -1
+                self.retrocede_cursor(pos -  1)
+
+    def move_cursor_inicio(self) -> None:
+        self.__cursor = self.__primeiro
+
+    def move_cursor_final(self) -> None:
+        self.__cursor = self.__ultimo
+
+    def proximo_do_cursor(self) -> any:
         if self.__cursor.proximo and self.__cursor.proximo.valor:
             return self.__cursor.proximo
 
-    def __anterior_cursor(self) -> any:
+    def anterior_ao_cursor(self) -> any:
         if self.__cursor.anterior and self.__cursor.anterior.valor:
             return self.__cursor.anterior
 
-    @property
     def comprimento(self) -> int:
         return self.__n_elementos
 
     def vazia(self) -> bool:
-        return self.comprimento == 0
+        return self.comprimento() == 0
 
     def cheia(self) -> bool:
-        return self.comprimento == self.__max
+        return self.comprimento() == self.__max
 
     def acessa_atual(self) -> any:
         return self.__cursor.valor
 
-    def acessa_posicao(self, pos) -> any:
-        self.__move_cursor_posicao(pos)
+    def acessa_posicao(self, pos: int) -> any:
+        self.move_cursor_posicao(pos)
         return self.acessa_atual()
 
     def inserir_como_atual(self, valor) -> None:
@@ -93,7 +92,7 @@ class Lista:
         self.__avisa_se_cheia()
         elemento = Elemento(valor=valor, proximo=self.__cursor)
 
-        if anterior := self.__anterior_cursor():
+        if anterior := self.anterior_ao_cursor():
             anterior = elemento
         else:
             self.__primeiro = elemento
@@ -108,7 +107,7 @@ class Lista:
         self.__avisa_se_cheia()
         elemento = Elemento(valor=valor, anterior=self.__cursor)
 
-        if proximo := self.__proximo_cursor():
+        if proximo := self.proximo_do_cursor():
             proximo.anterior = elemento
             elemento.proximo = proximo
         else:
@@ -119,23 +118,23 @@ class Lista:
         self.__n_elementos += 1
 
     def inserir_como_ultimo(self, valor) -> None:
-        self.__move_cursor_final()
+        self.move_cursor_final()
         self.inserir_apos_atual(valor)
 
     def inserir_como_primeiro(self, valor) -> None:
-        self.__move_cursor_inicio()
+        self.move_cursor_inicio()
         self.inserir_antes_atual(valor)
 
     def inserir_posicao(self, pos, valor) -> None:
         self.__avisa_se_cheia()
 
-        self.__move_cursor_posicao(pos)
+        self.move_cursor_posicao(pos)
         self.inserir_como_atual(valor)
 
     def excluir_atual(self) -> None:
         atual = self.__cursor
-        anterior = self.__anterior_cursor()
-        proximo = self.__proximo_cursor()
+        anterior = self.anterior_ao_cursor()
+        proximo = self.proximo_do_cursor()
 
         if anterior and proximo:
             anterior.proximo = proximo
@@ -153,42 +152,42 @@ class Lista:
         self.__n_elementos -= 1
 
     def excluir_primeiro(self) -> None:
-        self.__move_cursor_inicio()
+        self.move_cursor_inicio()
         self.excluir_atual()
 
     def excluir_ultimo(self) -> None:
-        self.__move_cursor_final()
+        self.move_cursor_final()
         self.excluir_atual()
 
     def excluir_posicao(self, pos) -> None:
-        self.__move_cursor_posicao(pos)
+        self.move_cursor_posicao(pos)
         self.excluir_atual()
 
     def excluir_elemento(self, valor) -> None:
         # Excluir todos os elementos da lista com determinado valor
 
-        self.__move_cursor_inicio()
-        n = self.comprimento
+        self.move_cursor_inicio()
+        n = self.comprimento()
         for i in range(n):
             if self.acessa_atual() == valor:
                 self.excluir_atual()
             else:
-                self.__avanca_cursor(1)
+                self.avanca_cursor(1)
 
     def busca(self, valor) -> bool:
-        self.__move_cursor_inicio()
+        self.move_cursor_inicio()
 
-        for i in range(self.comprimento):
+        for i in range(self.comprimento()):
             if self.acessa_atual() == valor:
                 return True
-            self.__avanca_cursor(1)
+            self.avanca_cursor(1)
 
         return False
 
     def posicao_de(self, valor) -> int:
-        self.__move_cursor_inicio()
+        self.move_cursor_inicio()
 
-        for i in range(self.comprimento):
+        for i in range(self.comprimento()):
             if self.acessa_atual() == valor:
                 return i
-            self.__avanca_cursor(1)
+            self.avanca_cursor(1)
